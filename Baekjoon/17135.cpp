1138b8ba4b959dd9430fee52ed6569404d8efcf4
ziwonii24 <32199644+ziwonii24@users.noreply.github.com>
2019-04-12 16:33:19 +0900

    Update 캐슬 디펜스

--- Baekjoon/17135.cpp
+++ Baekjoon/17135.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 void init() {
@@ -9,38 +6,23 @@ void init() {
 	cin.tie(0); cout.tie(0);
 }
 
-const int MAX = 15;
-int n, m, d;
-int arr[MAX + 2][MAX + 2], vis[MAX + 2][MAX + 2], tmp[MAX + 2][MAX + 2];
-bool check[MAX + 2], army[MAX + 2][MAX + 2];
+struct ARMY {
+	int xx, yy, dd;
+};
+
+const int MAX = 15 + 2;
+int n, m, D;
+int arr[MAX][MAX], vis[MAX][MAX], tmp[MAX][MAX];
+int dx[3] = { 0,0,-1 };
+int dy[3] = { 1,-1,0 };	//왼쪽, 오른쪽, 위쪽
+bool check[MAX];
 vector<int> v;
-int dx[3] = { -1,0,0 };
-int dy[3] = { 0,1,-1 };
-int ans, cnt;
-
-void copy_arr() {	//tmp에 arr복사(궁수배치마다 적 얼마나 죽일지 모르니깐)
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			tmp[i][j] = arr[i][j];
-}
-
-int remain_army() {	//적이 남아있는지 확인
-	int cnt_army = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (tmp[i][j] == 1)
-				cnt_army++;
-	return cnt_army;
-}
-
-void init_vis() {	//vis배열 초기화
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			vis[i][j] = 0;
-}
+vector<ARMY> army;
+vector<ARMY> army_kill;
+int kill_cnt, ans;
 
 void print_vis() {
-	cout << '\n';
+	cout << "vis======================\n";
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++)
 			cout << vis[i][j] << ' ';
@@ -49,83 +31,8 @@ void print_vis() {
 	cout << '\n';
 }
 
-void print_army() {
-	cout << '\n';
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			cout << army[i][j] << ' ';
-		cout << '\n';
-	}
-	cout << '\n';
-}
-
-void bfs(int y) {		//사정거리내 죽일 적 표시하기 위함
-	cout << "*궁수 열번호 = " << y << '\n';
-
-	queue<pair<int, int>> q;
-	q.push({ n - 1, y });
-	vis[n - 1][y] = 1;
-
-	cout << "*\n";
-	print_vis();
-
-	int xx = MAX + 2, yy = MAX + 2, dd = MAX + 2;	//현재 궁수가 쏘게 될 적의 위치
-
-	if (tmp[n - 1][y] == 1) {	
-		xx = n - 1; 
-		yy = y;
-		dd = vis[n - 1][y];
-	}
-
-	cout << "*xx=" << xx << ", yy=" << yy << ", dd=" << dd << '\n';
-
-	while (!q.empty()) {
-		int x = q.front().first;
-		y = q.front().second;
-		q.pop();
-
-		cout << "x=" << x << ", y=" << y << '\n';
-		print_vis();
-
-		if (vis[x][y] == d) {	//공격할 수 있는 거리까지 모두 본상태
-			cout << "xx=" << xx << ", yy=" << yy << ", dd=" << dd << '\n';
-			if (xx != MAX + 2 && yy != MAX + 2) {	//쏠 적이 있으면 표시
-				army[xx][yy] = true;
-
-				print_army();
-			}
-			return;
-		}
-
-		for (int k = 0; k < 3; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-
-			cout << "nx=" << nx << ", ny=" << ny << '\n';
-
-			if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-				if (vis[nx][ny] == 0) {	//공격할 수 있는 범위까지 넓혀가기 위함
-					vis[nx][ny] = vis[x][y] + 1;	
-					q.push({ nx, ny });
-
-					print_vis();
-
-					if (tmp[nx][ny] == 1) {	
-						if (vis[nx][ny] <= dd && ny < yy) {//가장 가깝고 왼쪽에 있는 적인지 확인
-							xx = nx;
-							yy = ny;
-							dd = vis[nx][ny];
-							cout << "xx=" << xx << ", yy=" << yy << ", dd=" << dd << '\n';
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
 void print_tmp() {
-	cout << '\n';
+	cout << "tmp======================\n";
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++)
 			cout << tmp[i][j] << ' ';
@@ -134,26 +41,100 @@ void print_tmp() {
 	cout << '\n';
 }
 
-void update_tmp() {
-	cout << "표시된 적\n";
-	print_army();
+void print_army() {
+	cout << "army=====================\n";
+	if (army.empty()) cout << "amry is empty\n";
+	else {
+		for (int i = 0; i < army.size(); i++)
+			cout << army[i].xx << ", " << army[i].yy << ", " << army[i].dd << '\n';
+		cout << '\n';
+	}
+}
 
-	//표시된 적 없애기
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (army[i][j]) {
-				tmp[i][j] = 0;
-				cnt++;
+void print_armykill() {
+	cout << "armykill=================\n";
+	if (army_kill.empty()) cout << "amry_kill is empty\n";
+	else {
+		for (int i = 0; i < army_kill.size(); i++)
+			cout << army_kill[i].xx << ", " << army_kill[i].yy << ", " << army_kill[i].dd << '\n';
+		cout << '\n';
+	}
+}
+
+bool comp(const ARMY a, const ARMY b) {
+	return (a.dd < b.dd) || (a.dd == b.dd && a.yy < b.yy);
+}
+
+void copy_arr() {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			tmp[i][j] = arr[i][j];
+}
+
+void init_vis() {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			vis[i][j] = 0;
+}
+
+void bfs(int y) {	//사정거리내에 있는 적의 위치를 찾아내기
+	queue<pair<int, int>> q;
+	q.push({ n - 1, y });
+	vis[n - 1][y] = 1;	//궁수 바로 위부터 방문
+	ARMY a;
+
+	if (tmp[n - 1][y] == 1) {
+		a.xx = n - 1;
+		a.yy = y;
+		a.dd = 1;
+		army.push_back(a);
+	}
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		y = q.front().second;
+		q.pop();
+
+		//사정거리 다 돌았으면 끝
+		if (vis[x][y] == D) return;
+
+		for (int k = 0; k < 3; k++) {
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+				if (vis[nx][ny] == 0) {
+					vis[nx][ny] = vis[x][y] + 1;
+					q.push({ nx, ny });
+				}
+
+				if (tmp[nx][ny] == 1) {
+					a.xx = nx;
+					a.yy = ny;
+					a.dd = vis[nx][ny];
+					army.push_back(a);
+				}
 			}
 		}
 	}
+}
 
-	cout << "cnt=" << cnt << '\n';
+void update_tmp() {
+	//적 죽이기
+	for (int i = 0; i < army_kill.size(); i++) {	//최소 0개 ~ 최대 3개
+		int x = army_kill[i].xx;
+		int y = army_kill[i].yy;
 
-	cout << "적군 전진 전 \n";
-	print_tmp();
+		if (tmp[x][y] == 1) {
+			tmp[x][y] = 0;
+			kill_cnt++;
+		}
+	}
 
-	//적군 한칸 전진
+	/*cout << "죽이고 난 후\n";
+	print_tmp();*/
+
+	//적 한 칸 전진
 	for (int i = n - 1; i >= 0; i--) {
 		for (int j = m - 1; j >= 0; j--) {
 			if (tmp[i][j] == 1) {
@@ -163,44 +144,63 @@ void update_tmp() {
 		}
 	}
 
-	
+	/*cout << "전진 한 후\n";
+	print_tmp();*/
 }
 
-void init_army() {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			army[i][j] = false;
+int remain_army() {	//적이 몇명남았는지 세는 함수
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (tmp[i][j] == 1)
+				cnt++;
+		}
+	}
+	return cnt;
 }
 
-void dfs(int x) {	//궁수 배치(열 번호만)
-	if (v.size() == 3) {
-		init_army();
-		init_vis();
-		copy_arr();
+void dfs(int x) {
+	if (v.size() == 3) {	//궁수 세명 선택
+		/*cout << "궁수 새로 배치\n";
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << ' ';
+		cout << '\n';*/
+
+		copy_arr();	//판새로짜기
 
 		while (1) {
-			cout << "*적군의 현황\n";
-			print_tmp();
-
-			cout << "*남은 적군의 수 = " << remain_army() << '\n';
-			if (remain_army() == 0) {
-				ans = max(ans, cnt);
-				cnt = 0;
+			if (remain_army() == 0) {	//남아있는 적이 없으면
+				//cout << "이 궁수배치로 죽인 적의 수 = " << kill_cnt << '\n';
+				ans = max(ans, kill_cnt);
+				kill_cnt = 0;
 				break;
 			}
 
 			for (int i = 0; i < v.size(); i++) {
-				init_vis();
-				bfs(v[i]);	//궁수 한명에 대해 쏠 적 찾기
+				init_vis();	//궁수 한명에 대한 vis배열이라서 초기화
+				army.clear();	//궁수 한명에 대한 죽일 적 후보라서 초기화
+				bfs(v[i]);
+
+				/*print_vis();
+				print_army();*/
+
+				if (!army.empty()) {
+					sort(army.begin(), army.end(), comp);
+					army_kill.push_back(army.front());//제일가깝고왼쪽에 있는 적만
+				}
+
+				//print_armykill();
 			}
 
-			update_tmp();
-			init_army();
-		}		
+			//bfs다 돌고 죽일 적 다 고르고 난 후
+			update_tmp();	//적 죽이고 적 한칸 전진
+			army_kill.clear();
+		}
 
 		return;
 	}
 
+	//궁수 세명 고르기
 	for (int i = x; i < m; i++) {
 		if (!check[i]) {
 			check[i] = true;
@@ -214,7 +214,7 @@ void dfs(int x) {	//궁수 배치(열 번호만)
 
 int main() {
 	init();
-	cin >> n >> m >> d;
+	cin >> n >> m >> D;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
 			cin >> arr[i][j];
