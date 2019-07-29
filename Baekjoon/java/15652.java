88e0342b10ce4fc5045afe6be2ee8e06dd1484ca
ziwonii24 <32199644+ziwonii24@users.noreply.github.com>
2019-07-29 15:53:43 +0900

    N과 M (4)

---
+++ Baekjoon/java/15652.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+	
+	StringBuilder sb = new StringBuilder();
+	int n, m;
+	ArrayList<Integer> arr = new ArrayList<>();
+	boolean[] vis;
+	
+	private void init() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] nm = br.readLine().split(" ");
+			n = Integer.parseInt(nm[0]);
+			m = Integer.parseInt(nm[1]);
+			vis = new boolean[n];
+		} catch(IOException e) {}
+	}
+	
+	private void dfs(int x) {
+		if(arr.size()==m) {
+			for(int i=0; i<m; i++) {
+				sb.append(arr.get(i)).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=x; i<n; i++) {
+//			if(!vis[i]) {
+				vis[i] = true;
+				arr.add(i+1);
+				dfs(i);
+				vis[i] = false;
+				arr.remove(arr.size()-1);
+//			}
+		}
+	}
+	
+	private void solve() {
+		init();
+		dfs(0);
+		System.out.println(sb);
+	}
+	
+	public static void main(String[] args) {
+		new Main().solve();
+	}
+}
