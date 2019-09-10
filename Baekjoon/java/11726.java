ffe6a10ea4d479fadc830304d947d6c000354e17
ziwonii24 <ziwonii25@gmail.com>
2019-09-10 22:59:49 +0900

    Update 2×n 타일링

--- Baekjoon/java/11726.java
+++ Baekjoon/java/11726.java
@@ -1,17 +1,14 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
 	
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	int n;
-	int[] d;
 	
 	void solve() throws IOException {
-		n = Integer.parseInt(br.readLine());
-		d = new int[n+1];
-		d[1] = 1; d[2] = 2;
-		for(int i=3; i<=n; i++) {
+		int n = Integer.parseInt(br.readLine());
+		int[] d = new int[n+1];
+		d[0] = 1; d[1] = 1;
+		for(int i=2; i<=n; i++) {
 			d[i] = (d[i-1] + d[i-2]) % 10007;
 		}
 		System.out.println(d[n]);
