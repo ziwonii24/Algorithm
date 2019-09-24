6cc7ee31ff65aea6a058dba0c86610f6cad68917
ziwonii24 <ziwonii25@gmail.com>
2019-09-24 18:01:43 +0900

    냉장고

--- JUNGOL/1828.java
+++ JUNGOL/1828.java
@@ -1,59 +1,66 @@
+/**************************************************************
+    Problem: 1828
+    User: chjiiwon30
+    Language: Java
+    Result: Success
+    Time:102 ms
+    Memory:9032 kb
+****************************************************************/
+ 
+ 
 import java.util.*;
 import java.io.*;
 
+/**그리디*/
 public class Main {
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st;
-	StringBuilder sb = new StringBuilder();
-	int n;
-	List<Node> list = new ArrayList<>();
-	int start=-300, end=-300;	// 범위 밖으로 초기화
-	
-	void solve() throws IOException {
-		n = Integer.parseInt(br.readLine());
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			list.add(new Node(x, y));
-		}
-		Collections.sort(list, new Comparator<Node>() {
-
-			@Override
-			public int compare(Node o1, Node o2) {
-				return Integer.valueOf(o1.x).compareTo(o2.x);
-			}
-			
-		});
-		int cnt = 0;
-		for(Node item: list) {			
-			if(start==-300) {
-				cnt+=1;
-				start = item.x;
-				end = item.y;
-				continue;
-			}
-			
-			if(end < item.x) {
-				cnt+=1;
-				start = item.x;
-				end = item.y;
-			}
-		}
-		sb.append(cnt);
-		System.out.println(sb);
-	}
-	
-	class Node {
-		int x, y;
-		public Node(int x, int y ) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		new Main().solve();
-	}
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+         
+        List<Node> list = new ArrayList<>();
+         
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            list.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+         
+        Collections.sort(list, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                if(Integer.valueOf(o1.e).equals(o2.e))
+                    return Integer.valueOf(o1.s).compareTo(o2.s);
+                return Integer.valueOf(o1.e).compareTo(o2.e);
+            }           
+        });
+         
+        int ans = 0;
+        int end = -300;
+        for(Node time: list) {
+            int ts = time.s;
+            int te = time.e;
+             
+            if(end==-300) {
+                end = te;
+                ans += 1;
+                continue;
+            }
+             
+            if(end < ts) {
+                end = te;
+                ans += 1;
+            }
+        }
+         
+        System.out.println(ans);
+    }   
+     
+    private static class Node {
+        int s, e;
+        public Node(int s, int e) {
+            this.s = s;
+            this.e = e;
+        }
+    }
 }
