0eca0bec867ec76315a5678dc52495404fd13f7c
최지원 <ziwonii25@gmail.com>
2023-01-02 23:04:19 +0900

    평균 구하기

--- Programmers/12944.js
+++ Programmers/12944.js
@@ -1,9 +1,10 @@
 function solution(arr) {
-  //   var answer = 0;
-  //   arr.forEach((i) => (answer += i));
-  //   answer /= arr.length;
-  //   return answer;
-  return arr.reduce((a, b) => a + b) / arr.length;
+    var answer = 0;
+    arr.forEach((i) => (answer += i));
+    answer /= arr.length;
+    return answer;
+    // another solution
+    // return arr.reduce((a, b) => a + b) / arr.length;
 }
 
 console.log(solution([1, 2, 3, 4]));
