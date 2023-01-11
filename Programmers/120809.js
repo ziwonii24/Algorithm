0d9e6c81ccd7a93dfb0b6da459d033bffacb29c3
최지원 <ziwonii25@gmail.com>
2023-01-11 22:00:01 +0900

    배열 두 배 만들기

--- Programmers/120809.js
+++ Programmers/120809.js
@@ -2,4 +2,5 @@ function solution(numbers) {
     return numbers.map((n) => n * 2)
     // another solution
     // return numbers.reduce((a, b) => [...a, b * 2], []);
+    // https://dinn.github.io/javascript/js-reduce-spread/
 }
