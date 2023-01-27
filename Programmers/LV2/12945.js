aa999090538474f3d5b4013b22e1bd360ab18207
ziwonii24 <ziwonii25@gmail.com>
2023-01-27 15:55:01 +0900

    :art: UPDATE 피보나치 수열 - memoization

--- Programmers/LV2/12945.js
+++ Programmers/LV2/12945.js
@@ -2,14 +2,25 @@
   시간초과..
 */
 function solution(n) {
-    function fibo(x) {
-        if (x === 0) {
-            return 0;
-        }
-        if (x === 1) {
-            return 1;
-        }
-        return fibo(x - 2) + fibo(x - 1);
+  // 1. recursive = O(1.6^n)
+  //   function fibo(x) {
+  //     if (x === 0 || x === 1) {
+  //       return x;
+  //     }
+  //     return fibo(x - 2) + fibo(x - 1);
+  //   }
+
+  // 2. recursive + memoization = O(n)
+  const MAX_LEN = 100_001;
+  let memo = new Array(MAX_LEN).fill(0);
+  memo[1] = 1;
+  function fibo(x) {
+    if (x > 1 && x < MAX_LEN && !memo[x]) {
+      memo[x] = (fibo(x - 1) + fibo(x - 2)) % 1234567;
     }
-    return fibo(n) % 1234567;
+    return memo[x];
+  }
+  return fibo(n);
 }
+console.log(solution(5));
+// 0 1 1 2 3 5 8 ...
